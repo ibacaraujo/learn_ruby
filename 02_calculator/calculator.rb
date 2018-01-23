a9b67cbe3c72000ef5236614756dcf858f781252
Iago Breno <ibacaraujo@gmail.com>
2018-01-23 20:25:23 +0000

    Finish calculator exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
-#write your code here
+def add(n1, n2)
+	return n1 + n2
+end
+
+def subtract(n1, n2)
+	return n1 - n2
+end
+
+def sum(array)
+	sum = 0
+	array.each {|n| sum += n}
+	return sum
+end
+
+def multiply(array)
+	multiplication = 1
+	array.each {|n| multiplication *= n}
+	return multiplication
+end
+
+def power(b, e)
+	return b ** e
+end
+
+def factorial(n)
+	if n == 0
+		return 0
+	end
+	factorial = 1
+	for i in 1..n
+		factorial *= i
+	end
+	return factorial
+end
