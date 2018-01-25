e1e5dcbc99755ace156c89d8a73c75fcfe9887f8
Iago Breno <ibacaraujo@gmail.com>
2018-01-25 21:46:33 +0000

    Finish exercises pig_latin and book_titles

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
-#write your code here
+def translate(s)
+	s = s.split
+	r = []
+	s.each do |w|
+		puts(w)
+		if w[0..1] == 'qu'
+			r.push(w[2..w.length-1] + w[0..1] + 'ay')
+			next
+		end
+		if w[0..2] == 'squ'
+			r.push(w[3..w.length-1] + w[0..2] + 'ay')
+			next
+		end
+		if w[0] =~ /[bcdfghjklmnpqrstvwxyz]/
+			if w[1] =~ /[bcdfghjklmnpqrstvwxyz]/
+				if w[2] =~ /[bcdfghjklmnpqrstvwxyz]/
+					r.push(w[3..w.length-1] + w[0..2] + 'ay')
+					next
+				end
+				r.push(w[2..w.length-1] + w[0..1] + 'ay')
+				next
+			end
+			r.push(w[1..w.length-1] + w[0] + 'ay')
+		else
+			r.push(w + 'ay')
+		end
+	end
+	r = r.join(" ")
+	return r
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(title)
+		title_words = title.split
+		new_title = []
+		conjunctions = ['and', 'or']
+		prepositions = ['in', 'of']
+		articles = ['the', 'a', 'an']
+		title_words.each_with_index do |title_word, i|
+			if i == 0 and (conjunctions.include? title_word or prepositions.include? title_word or articles.include? title_word)
+				title_word = title_word[0].upcase + title_word[1..title_word.length-1]
+				new_title.push(title_word)
+				next
+			end
+			if conjunctions.include? title_word or prepositions.include? title_word or articles.include? title_word
+				new_title.push(title_word)
+				next
+			end
+			title_word = title_word[0].upcase + title_word[1..title_word.length-1]
+			new_title.push(title_word)
+		end
+		@title = new_title.join(" ")
+	end
 end
